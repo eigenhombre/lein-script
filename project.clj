e367bfb41ee78bb588e744692349c0fb4f841f8c
John Jacobsen <john@mail.npxdesigns.com>
2014-02-16 12:48:27 -0600

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-script/lein-script "0.0.1"
+(defproject lein-script/lein-script "0.0.2"
   :description "Leiningen plugin/template for generating stand-alone scripts with lein-exec"
   :url "http://github.com/eigenhombre/lein-script"
   :license {:name "The MIT License (MIT)"
