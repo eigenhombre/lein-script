b2ad7389114f2f19a55ae301b6c70609a073820f
John Jacobsen <john@mail.npxdesigns.com>
2014-02-16 12:54:22 -0600

    Spruce up project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject lein-script/lein-script "0.0.2"
-  :description "Leiningen plugin/template for generating stand-alone scripts with lein-exec"
+  :description "Leiningen plugin/template for generating stand-alone Clojure scripts"
   :url "http://github.com/eigenhombre/lein-script"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [lein-exec "0.3.1"]]
   :min-lein-version "2.0.0"
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+         :url "https://github.com/eigenhombre/lein-script"})
